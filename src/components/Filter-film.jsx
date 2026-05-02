1356dc9962bd3406237af786e9dcd9579c8a71aa
unknown <imadesafsafi@gmail.com>
2026-05-02 17:33:27 +0200

    Some changes in select

--- src/components/Filter-film.jsx
+++ src/components/Filter-film.jsx
@@ -2,7 +2,7 @@ function Filter({genres , selectedGenre , setSelectedGenre}) {
     return(
         <section className="filter-section">
             <h3>filter by Genre</h3>
-            <select className=""
+            <select className="select-Style"
             value={selectedGenre}
             onChange={(e) => setSelectedGenre(e.target.value)}
             >
